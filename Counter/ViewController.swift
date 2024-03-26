a1b353eaa1eaa03bf16f97bd6576aa32db2c3b55
Sorokin Nikita <NikitoxicTheBest@yandex.ru>
2024-03-26 14:23:40 +0300

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var value: Int = 0
+    
+    @IBOutlet weak var tapMe: UIButton!
+    @IBOutlet weak var counterValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterValue.text = "Значение счётчика: \(value)"
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        value += 1
+        counterValue.text = "Значение счётчика: \(value)"
+    }
 }
-
